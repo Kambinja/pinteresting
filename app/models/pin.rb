0b4c25973594929c852b760906a351860617a689
Kambinja <ckmendes6@gmail.com>
2014-10-15 17:47:14 +0100

    added name to users

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,7 @@ class Pin < ActiveRecord::Base
 	belongs_to :user
 	has_attached_file :image, :styles => { :medium => "350x350>"} 
 		validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
+	validates :image, presence: true
+  validates :description, presence: true
 end
